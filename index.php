6214675f6674c1f452a04601a57dae744a46da36
bugakovAM <aleksbug19@gmail.com>
2022-08-31 01:53:47 +0300

    css и написание основного кода

--- index.php
+++ index.php
@@ -14,8 +14,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Троицкая пекарня - главная страница</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
 </head>
+
+
+
+
+
+
+
+
+
+
+
+
 <body>
-    м
+<header class="header">
+
+<div class="container">
+    <a href="/" class="logo">LOGO</a>
+    <nav>
+    <ul>
+        <li><a href="">главная</a></li> 
+        <li><a href="">Продукция</a></li>
+        <li><a href="">Новости</a></li>
+        <li><a href="">О нас</a></li>
+
+    </ul>
+    </nav>
+  </div>
+
+
+</header>
+
+<div class="main">
+    <div class="container">
+        <div id="hello"><p>Добро пожаловать</p> </div>
+        <div id="hello"><p>Добро пожаловать</p> </div>
+    </div>
+
+</div>
+
+
+
+<footer><div class="container"></div></footer>
 </body>
+
+
+
+
+
+
 </html>
