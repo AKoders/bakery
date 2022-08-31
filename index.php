0a5538bb118eb81a10708ba38e108c716c268840
bugakovAM <aleksbug19@gmail.com>
2022-09-01 01:17:26 +0300

    fix menu contacts and size menu

--- index.php
+++ index.php
@@ -31,18 +31,35 @@
 <body>
 <header class="header">
 
-<div class="container">
-    <a href="/" class="logo">LOGO</a>
-    <nav>
-    <ul>
-        <li><a href="">главная</a></li> 
-        <li><a href="">Продукция</a></li>
-        <li><a href="">Новости</a></li>
-        <li><a href="">О нас</a></li>
+    <div class="container">
+        <a href="/" class="logo"><img src="img/logo.png" alt="Троицкая пекарня" title="Троицкая пекарня"></a>
+        <div id="phones_bakery">
+            
+            <img id="icon_phone" src="img/home_phone_icon.png" title="Мобильный телефон">
+            <a id="telephone" href="tel:+3805011111111" title="Совершить звонок">+3805011111111</a>
+           
+            <img id="icon_phone" src="img/viber_icon.png" title="Наш вайбер">
+            <a id="telephone" href="viber://chat?number=+3805011111111" title="Начать чат">+3805011111111</a>
 
-    </ul>
-    </nav>
-  </div>
+            <img id="icon_phone" src="img/telegram_icon.png" title="Наш телеграм">
+            <a id="telephone" href="tg://resolve?domain=bakery_troitskbot" title="Начать чат">bakery_troitskbot</a>
+
+            <img id="icon_phone" src="img/mail_icon.png" title="Наша почта">
+            <a id="telephone" href="mailto:flpdet@mail.ru" title="Написать письмо">flpdet@mail.ru</a>
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">главная</a></li> 
+                <li><a href="">Продукция</a></li>
+                <li><a href="">Новости</a></li>
+                <li><a href="">О нас</a></li>
+
+            </ul>
+        </nav>
+
+
+    </div>
 
 
 </header>
